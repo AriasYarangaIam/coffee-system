69bbd480fd0d7d062e01eb8fd84e9c36bf85653c
Jona-9 <jonathantl09s@gmail.com>
2026-04-30 19:26:15 -0500

    Agregando interfaz Repository

---
+++ backend/src/main/java/com/coffee/backend/repository/AlmacenRepository.java
@@ -0,0 +1,8 @@
+package com.coffee.backend.repository;
+
+import com.coffee.backend.entity.Almacenes;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlmacenRepository extends JpaRepository<Almacenes,Long>{}
---
+++ backend/src/main/java/com/coffee/backend/repository/InsumoRepository.java
@@ -0,0 +1,8 @@
+package com.coffee.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InsumoRepository extends JpaRepository<InsumoRepository,Long> {
+}
